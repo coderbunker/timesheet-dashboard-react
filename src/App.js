3d5d28e8983c1a22edc3adc4490febb326fe1b6f
Alex Moran <mattermoran@gmail.com>
2018-05-12 19:58:14 +0800

    finish layout

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Logo from './images/coderbunker-logo.svg';
 
 import User from './images/blue_singleUser.svg';
 import Chart from './images/blue_chart.svg';
-import Blue_Yuan from './images/blue_yen.svg';
-import Gray_Yuan from './images/gray_yen.svg';
-import Multi_User from './images/blue_multiUser.svg';
-import Calendar_Clock from './images/blue_calendar:clock.svg';
-import Gray_Calendar from './images/gray_calendar.svg';
-import Gray_Clock from './images/gray_clock.svg';
+import BlueYuan from './images/blue_yen.svg';
+import GrayYuan from './images/gray_yen.svg';
+import MultiUser from './images/blue_multiUser.svg';
+import CalendarClock from './images/blue_calendar:clock.svg';
+import GrayCalendar from './images/gray_calendar.svg';
+import GrayClock from './images/gray_clock.svg';
 
 class App extends Component {
   render() {
@@ -28,43 +28,39 @@ class App extends Component {
           <div className="first-block">
             <div className="first-mini-containers">
               <div className="first-icon-containers">
-                <img className="icon" src={User} alt=""/>
-                <div className="first-numbers">14</div>
+                <img className="icon" src={User} alt="" />
+                <div className="first-numbers" style={{ paddingLeft: '5px' }}>
+                  14
+                </div>
               </div>
-              <div className="first-words-containers">
-                Active freelancers
-              </div>
+              <div className="first-words-containers">Active freelancers</div>
             </div>
-            <div className="divider"/>
+            <div className="divider" />
             <div className="first-mini-containers">
               <div className="first-icon-containers">
-                <img className="icon" src={Chart} alt=""/>
+                <img className="icon" src={Chart} alt="" />
                 <div className="first-numbers">22</div>
               </div>
-              <div className="first-words-containers">
-                Ongoing projects count with billable hours
-              </div>
+              <div className="first-words-containers">Ongoing projects count with billable hours</div>
             </div>
-            <div className="divider"/>
+            <div className="divider" />
             <div className="first-mini-containers">
               <div className="first-icon-containers">
-                <img className="icon" src={Blue_Yuan} alt="" />
+                <img className="icon" src={BlueYuan} alt="" />
                 <div className="first-numbers">000,000</div>
               </div>
-              <div className="first-words-containers">
-                Ongoing projects gross
-              </div>
+              <div className="first-words-containers">Ongoing projects gross</div>
             </div>
           </div>
 
           <div className="second-block">
             <div className="second-mini-container">
               <div className="second-icon-container">
-                <img className="icon-gray-yuan" src={Gray_Yuan} alt=""/>
-                <div className="second-number">000,000</div>
+                <img className="icon-gray-yuan" src={GrayYuan} alt="" />
               </div>
               <div className="second-words-container">
-                  Total Gross Billed(RMB)
+                <div className="second-number">2,134,243</div>
+                <div>Total Gross Billed(RMB)</div>
               </div>
             </div>
           </div>
@@ -72,64 +68,63 @@ class App extends Component {
           <div className="third-block">
             <div className="third-mini-containers">
               <div className="third-icon-containers">
-                <img className="icon" src={Multi_User} alt=""/>
+                <img className="icon" src={MultiUser} alt="" />
                 <div className="third-numbers">50</div>
               </div>
-              <div className="third-words-containers">
-                Freelancers with billable hours
-              </div>
+              <div className="third-words-containers">Freelancers with billable hours</div>
             </div>
-            <div className="third-divider"/>
+            <div className="third-divider" />
             <div className="third-mini-containers">
               <div className="third-icon-containers">
-                <img className="icon" src={Chart} alt=""/>
+                <img className="icon" src={Chart} alt="" />
                 <div className="third-numbers">22</div>
               </div>
-              <div className="third-words-containers">
-                Total Projects with billable hours
-              </div>
+              <div className="third-words-containers">Total Projects with billable hours</div>
             </div>
-            <div className="third-divider"/>
+            <div className="third-divider" />
             <div className="third-mini-containers">
               <div className="third-icon-containers">
-                <img className="icon" src={Calendar_Clock} alt=""/>
+                <img className="icon" src={CalendarClock} alt="" />
                 <div className="third-numbers">88</div>
               </div>
-              <div className="third-words-containers">
-                Total engineering months
-              </div>
+              <div className="third-words-containers">Total engineering months</div>
             </div>
           </div>
           <div className="last-block">
             <div className="last-inner-container">
               <div className="last-mini-containers">
                 <div className="last-icon-containers">
-                  <img className="icon" src={Gray_Calendar} alt=""/>
-                  <div className="last-numbers">2018-04-11</div>
+                  <div className="last-icon-inner-container">
+                    <img className="icon" src={GrayCalendar} alt="" />
+                    {/* dymamic number */}
+                    <div className="calendar-date">5</div>
+                  </div>
+                  {/* use moment.js for formatting date */}
+                  <div className="last-numbers">2018-04-05</div>
                 </div>
-                <div className="last-words-containers">
-                      Data start date
-                </div>
+                <div className="last-words-containers">Data start date</div>
               </div>
               <div className="last-mini-containers">
                 <div className="last-icon-containers">
-                  <img className="icon" src={Gray_Clock} alt=""/>
-                  <div className="last-numbers">2018-04-11</div>
+                  <img className="icon" src={GrayClock} alt="" />
+                  <div className="last-numbers">1 year 6 months 10 days</div>
                 </div>
-                <div className="last-words-containers">
-                      Elapsed since first billable hour
-                </div>
+                <div className="last-words-containers">Elapsed since first billable hour</div>
               </div>
               <div className="last-mini-containers">
                 <div className="last-icon-containers">
-                  <img className="icon" src={Gray_Calendar} alt=""/>
+                  <div className="last-icon-inner-container">
+                    <img className="icon" src={GrayCalendar} alt="" />
+                    {/* dymamic number */}
+                    <div className="calendar-date">11</div>
+                  </div>
+                  {/* use moment.js for formatting date */}
                   <div className="last-numbers">2018-04-11</div>
                 </div>
-                <div className="last-words-containers">
-                      Data last updated
-                </div>
+                <div className="last-words-containers">Data last updated</div>
               </div>
             </div>
+            <footer className="footer" />
           </div>
         </div>
       </div>
