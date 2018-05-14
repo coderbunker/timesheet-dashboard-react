f36cd93cf5eb82bfd06a216618bd9a52877db7c9
Alex Moran <mattermoran@gmail.com>
2018-05-15 00:04:22 +0800

    fix date and other stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import moment from 'moment';
 
 import { graphql } from 'react-apollo';
-// import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
+import './App.css';
 import Logo from './images/coderbunker-logo.svg';
 
 import User from './images/blue_singleUser.svg';
@@ -16,77 +16,99 @@ import CalendarClock from './images/blue_calendar:clock.svg';
 import GrayCalendar from './images/gray_calendar.svg';
 import GrayClock from './images/gray_clock.svg';
 
-import moment from 'moment';
-
-const OrgQuery = gql  `
-{
+const OrgQuery = gql`
+  {
     allOrganizations {
-    edges {
-      node {
-        orgname
-        since
-        activity
-        activePeopleCount
-        peopleCount
-        projectCount
-        totalGross
-        lastRefresh
-        lastUpdate
-        totalEngMonths
-        ongoingProjectCount
+      edges {
+        node {
+          orgname
+          since
+          activity
+          activePeopleCount
+          peopleCount
+          projectCount
+          totalGross
+          lastRefresh
+          lastUpdate
+          totalEngMonths
+          ongoingProjectCount
+        }
       }
     }
   }
-}
-` 
-const numberWithCommas = (x) => {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-    
+`;
+const numberWithCommas = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  dateDiff = date => {
+    const b = moment(date);
+    const a = moment();
+    const intervals = ['years', 'months', 'weeks', 'days'];
+    const out = [];
+
+    for (let i = 0; i < intervals.length; i++) {
+      const diff = a.diff(b, intervals[i]);
+      b.add(diff, intervals[i]);
+      out.push(`${diff} ${intervals[i]}`);
+    }
+    return out.join(' ');
+  };
+
   render() {
-    console.log(this.props)
-    if(this.props.data && this.props.data.loading) {
-      return<div style={{fontSize: "50px"}}>Loading.  Please Wait...</div>
+    console.log(this.props);
+    if (this.props.data && this.props.data.loading) {
+      return <div style={{ fontSize: '50px' }}>Loading. Please Wait...</div>;
     }
 
-    if(this.props.data && this.props.data.error) {
-      return<div>There was an error</div>
+    if (this.props.data && this.props.data.error) {
+      return <div>There was an error</div>;
     }
 
     const orgData = this.props.data.allOrganizations.edges[0].node;
+    const {
+      orgname,
+      peopleCount,
+      totalGross,
+      projectCount,
+      activePeopleCount,
+      ongoingProjectCount,
+      totalEngMonths,
+    } = orgData;
     console.log(orgData);
-    console.log(orgData.activity.substring(0, 21));
-    console.log(orgData.activity.substring(21, 33));
-    console.log(orgData.lastUpdate.substring(0, 10));
-    console.log(orgData.lastUpdate.substring(8, 10));
-    console.log(orgData.since.substring(0, 10));
-    console.log(orgData.since.substring(8, 10));
+
+    const lastUpdate = moment(orgData.lastUpdate).format('YYYY-MM-DD');
+    const lastUpdateDay = moment(orgData.lastUpdate).format('DD');
+    const sinceDate = moment(orgData.since).format('YYYY-MM-DD');
+    const sinceDateDay = moment(orgData.since).format('DD');
+    const since = this.dateDiff(orgData.since);
+
     return (
       <div>
-        {/*Begin Header*/}
+        {/* Begin Header */}
         <header>
           <div className="logo-line">
-            <div className="org-name">{orgData.orgname}</div>
+            <div className="org-name">{orgname}</div>
           </div>
           <div className="logo">
             <img src={Logo} alt="" />
           </div>
         </header>
-        {/*End Header*/}
+        {/* End Header */}
 
-        {/*Begin Content Container*/}
+        {/* Begin Content Container */}
         <div className="container">
-
-        {/*Begin First Block*/}
+          {/* Begin First Block */}
           <div className="first-block">
             <div className="first-mini-containers">
               <div className="first-icon-containers">
                 <img className="icon" src={User} alt="" />
                 <div className="first-numbers" style={{ paddingLeft: '5px' }}>
-                  {orgData.activePeopleCount}
+                  {activePeopleCount}
                 </div>
               </div>
               <div className="first-words-containers">Active freelancers</div>
@@ -95,9 +117,7 @@ class App extends Component {
             <div className="first-mini-containers">
               <div className="first-icon-containers">
                 <img className="icon" src={Chart} alt="" />
-                <div className="first-numbers">
-                  {orgData.ongoingProjectCount}
-                </div>
+                <div className="first-numbers">{ongoingProjectCount}</div>
               </div>
               <div className="first-words-containers">Ongoing projects count with billable hours</div>
             </div>
@@ -105,37 +125,33 @@ class App extends Component {
             <div className="first-mini-containers">
               <div className="first-icon-containers">
                 <img className="icon" src={BlueYuan} alt="" />
-                <div className="first-numbers"></div>
+                <div className="first-numbers" />
               </div>
               <div className="first-words-containers">Ongoing projects gross</div>
             </div>
           </div>
-          {/*End First Block*/}
-          
-          {/*Begin Second Block*/}
+          {/* End First Block */}
+
+          {/* Begin Second Block */}
           <div className="second-block">
             <div className="second-mini-container">
               <div className="second-icon-container">
                 <img className="icon-gray-yuan" src={GrayYuan} alt="" />
               </div>
               <div className="second-words-container">
-                <div className="second-number">
-                  {numberWithCommas(orgData.totalGross)}
-                </div>
+                <div className="second-number">{numberWithCommas(totalGross)}</div>
                 <div>Total Gross Billed(RMB)</div>
               </div>
             </div>
           </div>
-          {/*End Second Block*/}
+          {/* End Second Block */}
 
-          {/*Begin Third Block*/}
+          {/* Begin Third Block */}
           <div className="third-block">
             <div className="third-mini-containers">
               <div className="third-icon-containers">
                 <img className="icon" src={MultiUser} alt="" />
-                <div className="third-numbers">
-                  {orgData.peopleCount}
-                </div>
+                <div className="third-numbers">{peopleCount}</div>
               </div>
               <div className="third-words-containers">Freelancers with billable hours</div>
             </div>
@@ -143,9 +159,7 @@ class App extends Component {
             <div className="third-mini-containers">
               <div className="third-icon-containers">
                 <img className="icon" src={Chart} alt="" />
-                <div className="third-numbers">
-                  {orgData.projectCount}
-                </div>
+                <div className="third-numbers">{projectCount}</div>
               </div>
               <div className="third-words-containers">Total Projects with billable hours</div>
             </div>
@@ -153,35 +167,31 @@ class App extends Component {
             <div className="third-mini-containers">
               <div className="third-icon-containers">
                 <img className="icon" src={CalendarClock} alt="" />
-                <div className="third-numbers">
-                  {orgData.totalEngMonths}
-                </div>
+                <div className="third-numbers">{totalEngMonths}</div>
               </div>
               <div className="third-words-containers">Total engineering months</div>
             </div>
           </div>
-          {/*End Third Block*/}
+          {/* End Third Block */}
 
-          {/*Begin Fourth Block*/}
+          {/* Begin Fourth Block */}
           <div className="last-block">
             <div className="last-inner-container">
               <div className="last-mini-containers">
                 <div className="last-icon-containers">
                   <div className="last-icon-inner-container">
                     <img className="icon" src={GrayCalendar} alt="" />
-                    <div className="calendar-date"></div>
+                    <div className="calendar-date">{sinceDateDay}</div>
                   </div>
                   {/* use moment.js for formatting date */}
-                  <div className="last-numbers">{orgData.since}</div>
+                  <div className="last-numbers">{sinceDate}</div>
                 </div>
                 <div className="last-words-containers">Data start date</div>
               </div>
-              <div className="last-mini-containers">
+              <div className="last-mini-containers" style={{ width: '35%', textAlign: 'center' }}>
                 <div className="last-icon-containers">
                   <img className="icon" src={GrayClock} alt="" />
-                  <div className="last-numbers">
-                    {orgData.activity.substring(0, 21)}
-                  </div>
+                  <div className="last-numbers">{since}</div>
                 </div>
                 <div className="last-words-containers">Elapsed since first billable hour</div>
               </div>
@@ -190,36 +200,33 @@ class App extends Component {
                   <div className="last-icon-inner-container">
                     <img className="icon" src={GrayCalendar} alt="" />
                     {/* dymamic number */}
-                    <div className="calendar-date"></div>
+                    <div className="calendar-date">{lastUpdateDay}</div>
                   </div>
                   {/* use moment.js for formatting date */}
-                  <div className="last-numbers">
-                    {orgData.lastUpdate}
-                  </div>
+                  <div className="last-numbers">{lastUpdate}</div>
                 </div>
                 <div className="last-words-containers">Data last updated</div>
               </div>
             </div>
 
-            {/*Begin Footer*/}
+            {/* Begin Footer */}
             <footer className="footer" />
-            {/*End Footer*/}
+            {/* End Footer */}
           </div>
-          {/*End Fourth Block*/}
+          {/* End Fourth Block */}
         </div>
-        {/*End Content Container*/}
+        {/* End Content Container */}
       </div>
     );
   }
 }
 
-
-var AppWithData = graphql(OrgQuery, {
+const AppWithData = graphql(OrgQuery, {
   options: {
     variables: {
-      resource: ""
-    }
-  }
+      resource: '',
+    },
+  },
 })(App);
 
 export default AppWithData;
--- src/rootContainer.js
+++ src/rootContainer.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 
-import App from './App';
-
-//To get data from postgraphile server
+// To get data from postgraphile server
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 
-//creating the Apollo Client
+import App from './App';
+
+// creating the Apollo Client
 export const client = new ApolloClient({
-  link: new HttpLink( { uri: 'http://data.coderbunker.com/graphql' } ),
-  cache: new InMemoryCache()
+  link: new HttpLink({ uri: 'http://data.coderbunker.com/graphql' }),
+  cache: new InMemoryCache(),
 });
 
 export default class RootContainer extends Component {
-    render() {
-      return (
-        <ApolloProvider client={client}>
-          <App/>
-        </ApolloProvider>
-      )
-    }
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    );
+  }
 }
