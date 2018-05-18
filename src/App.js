b13c3bfe92f5f8c4a1a4699ae9dd63a487099ff8
Sam Evers <sam123evers@gmail.com>
2018-05-18 16:43:16 +0800

    initial push from sam after refactor

--- src/App.js
+++ src/App.js
@@ -16,12 +16,23 @@ import CalendarClock from './images/blue_calendar:clock.svg';
 import GrayCalendar from './images/gray_calendar.svg';
 import GrayClock from './images/gray_clock.svg';
 
-// const numberWithCommas = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+const numberWithCommas = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      backwardsMonthlyData: [],
+      orgData: {},
+      monthlyData: [],
+      totalGrossWithCommas: "",
+      bestMonth: Number,
+      lastUpdate: "",
+      lastUpdateDay: "",
+      sinceDate: "",
+      sinceDateDay: "",
+      since: ""
+    };
   }
 
   componentDidMount() {
@@ -35,9 +46,28 @@ class App extends Component {
     try {
       const allOrganizations = await _allOrganizations();
       const allMonthlyGrosses = await _allMonthlyGrosses();
-
-      // HERE YOU CAN DO ALL DATA MANIPULATION
-      console.log({ allOrganizations, allMonthlyGrosses });
+      const monthlyData = allMonthlyGrosses.data.allMonthlyGrosses.edges;
+      const orgData = allOrganizations.data.allOrganizations.edges[0].node;
+      const totalGrossWithCommas = numberWithCommas(orgData.totalGross);
+      const backwardsMonthlyData = [...monthlyData].reverse();
+      const bestMonth = Math.max(...monthlyData.map(i => i.node.total));
+      const lastUpdate = moment(orgData.lastUpdate).format('YYYY-MM-DD');
+      const lastUpdateDay = moment(orgData.lastUpdate).format('DD');
+      const sinceDate = moment(orgData.since).format('YYYY-MM-DD');
+      const sinceDateDay = moment(orgData.since).format('DD');
+      const since = this.dateDiff(orgData.since);
+      this.setState({
+        orgData,
+        monthlyData,
+        backwardsMonthlyData,
+        totalGrossWithCommas,
+        bestMonth,
+        lastUpdate,
+        lastUpdateDay,
+        sinceDate,
+        sinceDateDay,
+        since
+      });
     } catch (e) {
       console.log(e);
     }
@@ -58,9 +88,10 @@ class App extends Component {
   };
 
   render() {
-    return <div />;
-    // console.log(this.props);
-    // if (this.props.data && this.props.data.loading) {
+    console.log(this.state);
+    const s = this.state;
+    const o = this.state.orgData;
+    // if (this.state.data && this.props.data.loading) {
     //   return <div style={{ fontSize: '50px' }}>Loading. Please Wait...</div>;
     // }
 
@@ -68,185 +99,173 @@ class App extends Component {
     //   return <div>There was an error</div>;
     // }
 
-    // const monthlyData = this.props.data.allMonthlyGrosses.edges;
-    // const clone = [monthlyData];
-    // const reversedMonthlyData = clone.reverse();
-    // console.log({ clone, monthlyData });
+    return (
+      <div>
+        {/* Begin Header */}
+        <header>
+          <div className="logo-line">
+            <div className="org-name">
+              {o.orgname}
+            </div>
+          </div>
+          <div className="logo">
+            <img src={Logo} alt="" />
+          </div>
+        </header>
+        {/* End Header */}
 
-    // const orgData = this.props.data.allOrganizations.edges[0].node;
-    // const {
-    //   orgname,
-    //   peopleCount,
-    //   totalGross,
-    //   projectCount,
-    //   activePeopleCount,
-    //   ongoingProjectCount,
-    //   totalEngMonths,
-    // } = orgData;
-    // console.log(orgData);
+        {/* Begin Content Container */}
+        <div className="container">
+          {/* Begin First Block */}
+          <div className="first-block">
+            <div className="first-mini-containers">
+              <div className="first-icon-containers">
+                <img className="icon" src={User} alt="" />
+                <div className="first-numbers" style={{ paddingLeft: '5px' }}>
+                  {o.activePeopleCount}
+                </div>
+              </div>
+              <div className="first-words-containers">Active freelancers</div>
+            </div>
+            <div className="divider" />
+            <div className="first-mini-containers">
+              <div className="first-icon-containers">
+                <img className="icon" src={Chart} alt="" />
+                <div className="first-numbers">
+                  {o.ongoingProjectCount}
+                </div>
+              </div>
+              <div className="first-words-containers">Ongoing projects count with billable hours</div>
+            </div>
+            <div className="divider" />
+            <div className="first-mini-containers">
+              <div className="first-icon-containers">
+                <img className="icon" src={BlueYuan} alt="" />
+                <div className="first-numbers">
+                  
+                </div>
+              </div>
+              <div className="first-words-containers">Ongoing projects gross</div>
+            </div>
+          </div>
+          {/* End First Block */}
 
-    // const lastUpdate = moment(orgData.lastUpdate).format('YYYY-MM-DD');
-    // const lastUpdateDay = moment(orgData.lastUpdate).format('DD');
-    // const sinceDate = moment(orgData.since).format('YYYY-MM-DD');
-    // const sinceDateDay = moment(orgData.since).format('DD');
-    // const since = this.dateDiff(orgData.since);
+          {/* Begin Second Block */}
+          <div className="second-block">
+            <div className="second-mini-container">
+              <div className="second-icon-container">
+                <img className="icon-gray-yuan" src={GrayYuan} alt="" />
+              </div>
+              <div className="second-words-container">
+                <div className="second-number">
+                  {s.totalGrossWithCommas}
+                </div>
+                <div>Total Gross Billed(RMB)</div>
+              </div>
+            </div>
+            <div className="graph-container">
+              {this.state.backwardsMonthlyData.map(m => (
+                <div className="monthly-data-container">
+                  
+                  <div
+                    className="month-total"
+                    style={{ height: `${parseFloat(m.node.total) / this.state.bestMonth * 100}%` }}
+                  />
+                  <div className="month-name">{m.node.entryMonthName}</div>
+                  
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* End Second Block */}
 
-    // const result = monthlyData.reduce(
-    //   (prev, next) => {
-    //     if (prev.highestTotal > parseFloat(next.node.total)) {
-    //       return prev;
-    //     }
-    //     return { highestTotal: parseFloat(next.node.total) };
-    //   },
-    //   { highestTotal: 0 }
-    // );
+          {/* Begin Third Block */}
+          <div className="third-block">
+            <div className="third-mini-containers">
+              <div className="third-icon-containers">
+                <img className="icon" src={MultiUser} alt="" />
+                <div className="third-numbers">
+                  {o.peopleCount}
+                </div>
+              </div>
+              <div className="third-words-containers">Freelancers with billable hours</div>
+            </div>
+            <div className="third-divider" />
+            <div className="third-mini-containers">
+              <div className="third-icon-containers">
+                <img className="icon" src={Chart} alt="" />
+                <div className="third-numbers">
+                  {o.projectCount}
+                </div>
+              </div>
+              <div className="third-words-containers">Total Projects with billable hours</div>
+            </div>
+            <div className="third-divider" />
+            <div className="third-mini-containers">
+              <div className="third-icon-containers">
+                <img className="icon" src={CalendarClock} alt="" />
+                <div className="third-numbers">
+                  {o.totalEngMonths}
+                </div>
+              </div>
+              <div className="third-words-containers">Total engineering months</div>
+            </div>
+          </div>
+          {/* End Third Block */}
 
-    // return (
-    //   <div>
-    //     {/* Begin Header */}
-    //     <header>
-    //       <div className="logo-line">
-    //         <div className="org-name">{orgname}</div>
-    //       </div>
-    //       <div className="logo">
-    //         <img src={Logo} alt="" />
-    //       </div>
-    //     </header>
-    //     {/* End Header */}
+          {/* Begin Fourth Block */}
+          <div className="last-block">
+            <div className="last-inner-container">
+              <div className="last-mini-containers">
+                <div className="last-icon-containers">
+                  <div className="last-icon-inner-container">
+                    <img className="icon" src={GrayCalendar} alt="" />
+                    <div className="calendar-date">
+                      {s.sinceDateDay}
+                    </div>
+                  </div>
+                  <div className="last-numbers">
+                    {s.sinceDate}
+                  </div>
+                </div>
+                <div className="last-words-containers">Data start date</div>
+              </div>
+              <div className="last-mini-containers" style={{ width: '35%', textAlign: 'center' }}>
+                <div className="last-icon-containers">
+                  <img className="icon" src={GrayClock} alt="" />
+                  <div className="last-numbers">
+                    {s.since}
+                  </div>
+                </div>
+                <div className="last-words-containers">Elapsed since first billable hour</div>
+              </div>
+              <div className="last-mini-containers">
+                <div className="last-icon-containers">
+                  <div className="last-icon-inner-container">
+                    <img className="icon" src={GrayCalendar} alt="" />
+                    
+                    <div className="calendar-date">
+                      {s.lastUpdateDay}
+                    </div>
+                  </div>
+                  
+                  <div className="last-numbers">
+                    {s.lastUpdate}
+                  </div>
+                </div>
+                <div className="last-words-containers">Data last updated</div>
+              </div>
+            </div>
 
-    //     {/* Begin Content Container */}
-    //     <div className="container">
-    //       {/* Begin First Block */}
-    //       <div className="first-block">
-    //         <div className="first-mini-containers">
-    //           <div className="first-icon-containers">
-    //             <img className="icon" src={User} alt="" />
-    //             <div className="first-numbers" style={{ paddingLeft: '5px' }}>
-    //               {activePeopleCount}
-    //             </div>
-    //           </div>
-    //           <div className="first-words-containers">Active freelancers</div>
-    //         </div>
-    //         <div className="divider" />
-    //         <div className="first-mini-containers">
-    //           <div className="first-icon-containers">
-    //             <img className="icon" src={Chart} alt="" />
-    //             <div className="first-numbers">{ongoingProjectCount}</div>
-    //           </div>
-    //           <div className="first-words-containers">Ongoing projects count with billable hours</div>
-    //         </div>
-    //         <div className="divider" />
-    //         <div className="first-mini-containers">
-    //           <div className="first-icon-containers">
-    //             <img className="icon" src={BlueYuan} alt="" />
-    //             <div className="first-numbers" />
-    //           </div>
-    //           <div className="first-words-containers">Ongoing projects gross</div>
-    //         </div>
-    //       </div>
-    //       {/* End First Block */}
-
-    //       {/* Begin Second Block */}
-    //       <div className="second-block">
-    //         <div className="second-mini-container">
-    //           <div className="second-icon-container">
-    //             <img className="icon-gray-yuan" src={GrayYuan} alt="" />
-    //           </div>
-    //           <div className="second-words-container">
-    //             <div className="second-number">{numberWithCommas(totalGross)}</div>
-    //             <div>Total Gross Billed(RMB)</div>
-    //           </div>
-    //         </div>
-    //         <div className="graph-container">
-    //           {monthlyData.map(m => (
-    //             // console.log(m)
-    //             // console.log(result.highestTotal)
-    //             <div className="monthly-data-container">
-    //               {/* <div className="special"> */}
-    //               <div
-    //                 className="month-total"
-    //                 style={{ height: `${parseFloat(m.node.total) / result.highestTotal * 100}%` }}
-    //               />
-    //               <div className="month-name">{m.node.entryMonthName}</div>
-    //               {/* </div> */}
-    //             </div>
-    //           ))}
-    //         </div>
-    //       </div>
-    //       {/* End Second Block */}
-
-    //       {/* Begin Third Block */}
-    //       <div className="third-block">
-    //         <div className="third-mini-containers">
-    //           <div className="third-icon-containers">
-    //             <img className="icon" src={MultiUser} alt="" />
-    //             <div className="third-numbers">{peopleCount}</div>
-    //           </div>
-    //           <div className="third-words-containers">Freelancers with billable hours</div>
-    //         </div>
-    //         <div className="third-divider" />
-    //         <div className="third-mini-containers">
-    //           <div className="third-icon-containers">
-    //             <img className="icon" src={Chart} alt="" />
-    //             <div className="third-numbers">{projectCount}</div>
-    //           </div>
-    //           <div className="third-words-containers">Total Projects with billable hours</div>
-    //         </div>
-    //         <div className="third-divider" />
-    //         <div className="third-mini-containers">
-    //           <div className="third-icon-containers">
-    //             <img className="icon" src={CalendarClock} alt="" />
-    //             <div className="third-numbers">{totalEngMonths}</div>
-    //           </div>
-    //           <div className="third-words-containers">Total engineering months</div>
-    //         </div>
-    //       </div>
-    //       {/* End Third Block */}
-
-    //       {/* Begin Fourth Block */}
-    //       <div className="last-block">
-    //         <div className="last-inner-container">
-    //           <div className="last-mini-containers">
-    //             <div className="last-icon-containers">
-    //               <div className="last-icon-inner-container">
-    //                 <img className="icon" src={GrayCalendar} alt="" />
-    //                 <div className="calendar-date">{sinceDateDay}</div>
-    //               </div>
-    //               {/* use moment.js for formatting date */}
-    //               <div className="last-numbers">{sinceDate}</div>
-    //             </div>
-    //             <div className="last-words-containers">Data start date</div>
-    //           </div>
-    //           <div className="last-mini-containers" style={{ width: '35%', textAlign: 'center' }}>
-    //             <div className="last-icon-containers">
-    //               <img className="icon" src={GrayClock} alt="" />
-    //               <div className="last-numbers">{since}</div>
-    //             </div>
-    //             <div className="last-words-containers">Elapsed since first billable hour</div>
-    //           </div>
-    //           <div className="last-mini-containers">
-    //             <div className="last-icon-containers">
-    //               <div className="last-icon-inner-container">
-    //                 <img className="icon" src={GrayCalendar} alt="" />
-    //                 {/* dymamic number */}
-    //                 <div className="calendar-date">{lastUpdateDay}</div>
-    //               </div>
-    //               {/* use moment.js for formatting date */}
-    //               <div className="last-numbers">{lastUpdate}</div>
-    //             </div>
-    //             <div className="last-words-containers">Data last updated</div>
-    //           </div>
-    //         </div>
-
-    //         {/* Begin Footer */}
-    //         <footer className="footer" />
-    //         {/* End Footer */}
-    //       </div>
-    //       {/* End Fourth Block */}
-    //     </div>
-    //     {/* End Content Container */}
-    //   </div>
-    // );
+            {/* Begin Footer */}
+            <footer className="footer" />
+            {/* End Footer */}
+          </div>
+          {/* End Fourth Block */}
+        </div>
+        {/* End Content Container */}
+      </div>
+    );
   }
 }
 
--- src/queries.js
+++ src/queries.js
@@ -1,7 +1,7 @@
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider, graphql } from 'react-apollo';
+// import { ApolloProvider, graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const client = new ApolloClient({
